04c75bbfbe79f2e48694b0127ff50e1d3cdd8295
bhavanibodduru <146819738+bhavanibodduru@users.noreply.github.com>
2024-07-18 13:58:33 +0530

    Create 4.Greatest_of_two_numbers.c

---
+++ 4.Greatest_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("enter the two numbers");
+    scanf("%d%d",&n1,&n2);
+    if(n1>n2)
+    printf("n1 is greater");
+    else
+    printf("n2 is greater");
+}
